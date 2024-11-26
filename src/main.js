85336f2503d51e63097c6353c8262fb2fb3c53f5
yanballas <yanballas@yandex.ru>
2024-11-27 02:12:12 +0300

    feat декстоп

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
 //styles
+import "swiper/css";
 import "./main.scss";
 
+//modules
+import Swiper from "swiper";
+import { Pagination, Autoplay } from "swiper/modules";
+
 document.addEventListener("DOMContentLoaded", () => {
-  const error = "Error in JS";
+  const error = new Error("Error in main.js");
+
+  const initialSlider = (classSelector) => {
+    const newSlider = new Swiper(`.${classSelector} .swiper`, {
+      loop: true,
+      spaceBetween: 32,
+      modules: [Pagination, Autoplay],
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: `.${classSelector} .swiper-pagination`,
+        clickable: true,
+        bulletClass: "swiper-pagination-bullet",
+        bulletActiveClass: "swiper-pagination-bullet-active",
+      },
+    });
+
+    if (newSlider) return newSlider;
+
+    return null;
+  };
+
+  const mainSlideSelector = "main__about-slider";
   try {
+    initialSlider(mainSlideSelector);
   } catch {
     console.error(error);
   }
