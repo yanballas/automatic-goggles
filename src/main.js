4cb1225f6443ea36ea8fc1202688c455ffbc0646
yanballas <yanballas@yandex.ru>
2024-11-27 12:30:44 +0300

    edit adaptive

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { Pagination, Autoplay } from "swiper/modules";
 
 document.addEventListener("DOMContentLoaded", () => {
   const error = new Error("Error in main.js");
+  const mainSlideSelector = "main__about-slider";
 
   const initialSlider = (classSelector) => {
     const newSlider = new Swiper(`.${classSelector} .swiper`, {
@@ -30,9 +31,106 @@ document.addEventListener("DOMContentLoaded", () => {
     return null;
   };
 
-  const mainSlideSelector = "main__about-slider";
+  const navigation = () => {
+    const nav = document.querySelector(".nav");
+    const openButton = document.querySelector(".header__burger button");
+    const closeButton = document.querySelector(".nav__button");
+
+    const handleClick = () => {
+      if (nav.classList.contains("nav__active"))
+        return nav.classList.remove("nav__active");
+
+      return nav.classList.add("nav__active");
+    };
+
+    openButton.addEventListener("click", handleClick);
+    closeButton.addEventListener("click", handleClick);
+  };
+
+  const formSubmit = () => {
+    const form = document.querySelector(".main__join-form form");
+    const inputs = Array.from(form.querySelectorAll("input"));
+    const submitButton = form.querySelector(".page__button");
+
+    const user = {
+      name: "",
+      surname: "",
+      email: "",
+      zip: "",
+    };
+
+    const handleInput = (e) => {
+      validateInput(e.target);
+      checkFormValidity();
+    };
+
+    const validateInput = (input) => {
+      const value = input.value.trim();
+      let isValid = false;
+
+      if (value) {
+        switch (input.name) {
+          case "name":
+            user.name = value;
+            isValid = true;
+            break;
+
+          case "surname":
+            user.surname = value;
+            isValid = true;
+            break;
+
+          case "email":
+            user.email = value;
+            isValid = true;
+            break;
+
+          case "zip":
+            user.zip = value;
+            isValid = true;
+            break;
+        }
+      }
+
+      return isValid;
+    };
+
+    const checkFormValidity = () => {
+      const isValid = inputs.every((input) => {
+        if (input.type === "checkbox") {
+          return input.checked;
+        }
+        return validateInput(input);
+      });
+
+      if (isValid) {
+        submitButton.classList.add("--active");
+      } else {
+        submitButton.classList.remove("--active");
+      }
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      checkFormValidity();
+
+      if (submitButton.classList.contains("--active")) {
+        alert(`отправлена форма ${JSON.stringify(user)}`);
+      } else {
+        alert('форма заполнена неправильно')
+      }
+    };
+
+    form.addEventListener("submit", handleSubmit);
+    inputs.forEach((input) => {
+      input.addEventListener("input", handleInput);
+    });
+  };
+
   try {
+    navigation();
     initialSlider(mainSlideSelector);
+    formSubmit();
   } catch {
     console.error(error);
   }
